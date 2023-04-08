2d135619b13820e870d7c7a2cfa00a37e6fd8041
Jordan Freundlich <jordan.freundlich@gmail.com>
2023-04-08 14:46:29 -0400

    started working on sort

--- src/components/Roster/RosterTable.tsx
+++ src/components/Roster/RosterTable.tsx
@@ -7,14 +7,52 @@ import {
   TableRow,
   Paper,
 } from "@mui/material";
+import { positions } from "@mui/system";
+import { useEffect, useState } from "react";
 import { Player } from "../Interfaces/Players";
 import { RosterRow } from "./RosterRow";
+import { headerToPropertyMap } from "./utlis/headerToPropertyMap";
 
 interface Props {
   players: Player[];
 }
 
 export const RosterTable = ({ players }: Props) => {
+  const [sortDir, setSortDir] = useState<"asc" | "desc">("desc");
+  const [sortCol, setSortCol] = useState<string>("Catching");
+  const [sortedPlayers, setSortedPlayers] = useState<Player[]>(players);
+
+  const handleSortDir = (col: string) => {
+    if (sortCol === col) {
+      setSortDir(sortDir === "asc" ? "desc" : "asc");
+    } else {
+      setSortCol(col);
+      setSortDir("asc");
+    }
+  };
+  useEffect(() => {
+    // const property = headerToPropertyMap[sortCol];
+    const property = "stats.receivingRatings.Catching";
+
+    if (property !== undefined) {
+      const sortedPlayers = [...players].sort((a, b) => {
+        console.log(a.stats.receivingRatings.Catching);
+
+        // const valueA = a.[property];
+        // const valueB = b.property;
+        // if (valueA < valueB) {
+        //   return sortDir === "asc" ? -1 : 1;
+        // }
+        // if (valueA > valueB) {
+        //   return sortDir === "asc" ? 1 : -1;
+        // }
+        // return 0;
+      });
+      setSortedPlayers(sortedPlayers);
+    }
+  }, [players, sortDir, sortCol]);
+
+  console.log(sortedPlayers);
   return (
     <TableContainer component={Paper}>
       <Table>
@@ -100,7 +138,7 @@ export const RosterTable = ({ players }: Props) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {players.map((player, index) => (
+          {sortedPlayers.map((player, index) => (
             <RosterRow key={index} player={player} />
           ))}
         </TableBody>
